919c85340a80823f8e1d44ca96a444e1a30f637e
Joao Carmassi <joaovitorcarmassi@gmail.com>
2026-06-18 09:21:48 -0300

    improve cardapio page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
